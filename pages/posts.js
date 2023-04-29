aee6841726ec05ff86f82845df82e03448d6c8ca
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2023-04-29 17:57:34 +0400

    feat: implement request for data with React useEffect

--- pages/posts.js
+++ pages/posts.js
@@ -1,11 +1,24 @@
 import MainLayout from "@/components/MainLayout";
+import { useState, useEffect } from "react";
 
 export default function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch("http://localhost:4200/posts");
+      const json = await response.json();
+      setPosts(json);
+    }
+    load();
+  }, []);
+
   return (
     <MainLayout title="Posts">
-        <title>Next.JS-1 Posts</title>
-        <meta charSet="UTF-8"></meta>
+      <title>Next.JS-1 Posts</title>
+      <meta charSet="UTF-8"></meta>
       <h1>Posts</h1>
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
     </MainLayout>
   );
 }
