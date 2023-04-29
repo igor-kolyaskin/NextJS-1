f20837332e2a9da6649d0bcf2416ea8b38ab81c7
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2023-04-29 19:47:38 +0400

    feat: complete page Post with request

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,11 +1,19 @@
 import MainLayout from "@/components/MainLayout";
-import { useRouter } from "next/router";
+import Link from "next/link";
 
-export default function Post() {
-  const router = useRouter();
+export default function Post({ post }) {
   return (
     <MainLayout title="Post">
-      <h1>Post {router.query.id}</h1>
+      <h1>{post.title}</h1>
+      <hr />
+      <p>{post.body}</p>
+      <button><Link href="/posts">Back to all posts</Link></button>
     </MainLayout>
   );
 }
+
+Post.getInitialProps = async (ctx) => {
+  const response = await fetch(`http://localhost:4200/posts/${ctx.query.id}`);
+  const post = await response.json();
+  return { post };
+};
