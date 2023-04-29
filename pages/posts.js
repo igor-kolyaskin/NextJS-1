0eadfa1ea8ec0c2d4a487cd13ce6235d0f78b025
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2023-04-29 19:15:00 +0400

    feat: replace render posts content as React with render content as Next

--- pages/posts.js
+++ pages/posts.js
@@ -1,24 +1,40 @@
 import MainLayout from "@/components/MainLayout";
+import Link from "next/link";
 import { useState, useEffect } from "react";
 
-export default function Posts() {
-  const [posts, setPosts] = useState([]);
+export default function Posts({ posts }) {
+  // const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    async function load() {
-      const response = await fetch("http://localhost:4200/posts");
-      const json = await response.json();
-      setPosts(json);
-    }
-    load();
-  }, []);
+  // useEffect(() => {
+  //   async function load() {
+  //     const response = await fetch("http://localhost:4200/posts");
+  //     const json = await response.json();
+  //     setPosts(json);
+  //   }
+  //   load();
+  // }, []);
 
   return (
     <MainLayout title="Posts">
       <title>Next.JS-1 Posts</title>
       <meta charSet="UTF-8"></meta>
       <h1>Posts</h1>
-      <pre>{JSON.stringify(posts, null, 2)}</pre>
+      <ul>
+        {posts.map((post) => {
+          return (
+            <li key={post.id}>
+              <Link href={`/post/${post.id}`}>{post.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+      {/* <pre>{JSON.stringify(posts, null, 2)}</pre> */}
     </MainLayout>
   );
 }
+
+Posts.getInitialProps = async () => {
+  const response = await fetch("http://localhost:4200/posts");
+  const posts = await response.json();
+  return { posts };
+};
