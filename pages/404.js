3868f91d5c2b9a081314c3f81c2f4b78b8df564f
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2023-04-29 17:31:21 +0400

    style: change css to scss

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,14 @@
+import MainLayout from "@/components/MainLayout";
 import Link from "next/link";
+import classes from "../styles/error.module.scss"
 
 export default function ErrorPage() {
   return (
-    <>
-      <h1>Error 404</h1>
+    <MainLayout>
+      <h1 className={classes.error}>Error 404</h1>
       <p>
         Please <Link href="/">go back</Link> to the home page
       </p>
-    </>
+    </MainLayout>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "../styles/globals.scss";
 // import "../styles/main.css"
 
 export default function MyApp({ Component, pageProps }) {
