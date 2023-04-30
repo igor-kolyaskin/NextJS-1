0f1c0768dc5034fee137b62208d9956dd0aea16a
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2023-04-30 11:09:49 +0400

    feat: implement loader, add server rendering for About page

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import "../styles/globals.scss";
+import NextNProgress from "nextjs-progressbar";
 // import "../styles/main.css"
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <NextNProgress
+        color="yellow"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
       <Component {...pageProps} />
       {/* <style jsx global>{`
         body {
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,14 +1,20 @@
 import MainLayout from "@/components/MainLayout";
 import Router from "next/router";
 
-export default function About() {
+export default function About({ title }) {
   const linkClickHandler = () => {
     Router.push("/posts");
   };
   return (
     <MainLayout title="About">
-      <h1>About</h1>
+      <h1>{title}</h1>
       <button onClick={linkClickHandler}>Go back to posts</button>
     </MainLayout>
   );
 }
+
+About.getInitialProps = async () => {
+  const response = await fetch("http://localhost:4200/about");
+  const data = await response.json();
+  return { title: data.title };
+};
