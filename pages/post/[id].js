d9b03d56ee0f6f80c094dd551a6978e5987ebd01
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2023-04-30 10:02:55 +0400

    feat: implement separat reqest for post loading - client side and server side

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,19 +1,48 @@
 import MainLayout from "@/components/MainLayout";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
-export default function Post({ post }) {
+export default function Post({ post: serverPost }) {
+  const [post, setPost] = useState(serverPost);
+  const router = useRouter();
+
+  useEffect(() => {
+    const load = async () => {
+      const response = await fetch(
+        `http://localhost:4200/posts/${router.query.id}`
+      );
+      const data = await response.json();
+      setPost(data);
+    };
+    if (!serverPost) load();
+  }, []);
+
+  if (!post) {
+    return (
+      <MainLayout>
+        <p>Loading...</p>
+      </MainLayout>
+    );
+  }
+
   return (
     <MainLayout title="Post">
       <h1>{post.title}</h1>
       <hr />
       <p>{post.body}</p>
-      <button><Link href="/posts">Back to all posts</Link></button>
+      <button>
+        <Link href="/posts">Back to all posts</Link>
+      </button>
     </MainLayout>
   );
 }
 
-Post.getInitialProps = async (ctx) => {
-  const response = await fetch(`http://localhost:4200/posts/${ctx.query.id}`);
+Post.getInitialProps = async ({ query, req }) => {
+  if (!req) {
+    return { post: null };
+  }
+  const response = await fetch(`http://localhost:4200/posts/${query.id}`);
   const post = await response.json();
   return { post };
 };
