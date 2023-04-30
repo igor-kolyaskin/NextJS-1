cae8b770bb5729752cfe80f5ddaec41ba5801345
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2023-04-30 11:30:47 +0400

    refactor: use out-of-the-box middleware for custom api

--- pages/api/echo/[id].js
+++ pages/api/echo/[id].js
@@ -1,5 +1,6 @@
 export default function getById(req, response) {
-  response.statusCode = 200;
-  response.setHeader("Content-Type", "application/json");
-  response.end(req.query.id);
+  //   response.statusCode = 200;
+  //   response.setHeader("Content-Type", "application/json");
+  //   response.end(req.query.id);
+  response.json({ yourId: req.query.id });
 }
