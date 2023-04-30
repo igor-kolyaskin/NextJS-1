bb20f70b49ea3a0813f6f3edc833054d87612659
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2023-04-30 13:06:33 +0400

    refactor: refactor page echo in accordance with TypeScript

--- pages/api/echo.tsx
+++ pages/api/echo.tsx
@@ -1,9 +1,12 @@
-export default function echo(req: {
-  query: { message: any; };
-}, response: {
-  statusCode: number;
-  setHeader: (arg0: string, arg1: string) => void; end: (arg0: string) => void;
-}) {
+import { NextApiRequest, NextApiResponse } from "next";
+
+interface MessageNextApiRequest extends NextApiRequest {
+  query: {
+    message?: string
+  }
+}
+
+export default function echo(req: NextApiRequest, response: NextApiResponse) {
   response.statusCode = 200;
   response.setHeader("Content-Type", "application/json");
   response.end(
